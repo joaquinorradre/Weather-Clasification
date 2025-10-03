a739315d1db6424ca742a9fe062f5f38ea0f2ee9
goicoechea.147720 <goicoechea.147720@e.unavarra.es>
2025-10-03 12:18:48 +0200

    fixed cnn_v2

--- weather_classification/modeling/cnn.py
+++ weather_classification/modeling/cnn.py
@@ -84,7 +84,7 @@ class CNN_V1_reg(nn.Module):
         x = self.classifier(x)
         return x
     
-    
+
 class CNN_V2(nn.Module):
     def __init__(self, input_dim, num_classes=11):
         super(CNN_V2, self).__init__()
@@ -93,13 +93,24 @@ class CNN_V2(nn.Module):
             nn.ReLU(),
             nn.Conv2d(32, 64, 5),
             nn.ReLU(),
+
             nn.MaxPool2d(2, 2),
+
             nn.Conv2d(64, 128, 3),
             nn.ReLU(),
-            nn.Conv2d(128, 512, 5),
+            nn.Conv2d(128, 256, 5),
             nn.ReLU(),
+
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(256, 512, 3),
+            nn.ReLU(),
+
             nn.MaxPool2d(3, 3)
         )
+
+        # Compute flattened feature size after convs
+        self.flattened_size = 512*8*8 
 
         # Compute flattened feature size after convs
         self.flattened_size = 512*7*7  
@@ -111,6 +122,58 @@ class CNN_V2(nn.Module):
             nn.ReLU(),
             nn.Linear(256, 128),
             nn.ReLU(),
+            nn.Linear(128, num_classes)
+        )
+        
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)  # flatten
+        x = self.classifier(x)
+        return x
+    
+class CNN_V2_reg(nn.Module):
+    def __init__(self, input_dim, num_classes=11):
+        super(CNN_V2_reg, self).__init__()
+        self.p = 0.4
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, 3),
+            nn.ReLU(),
+            nn.Dropout(self.p),
+            nn.Conv2d(32, 64, 5),
+            nn.ReLU(),
+            nn.Dropout(self.p),
+
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(64, 128, 3),
+            nn.ReLU(),
+            nn.Dropout(self.p),
+            nn.Conv2d(128, 256, 5),
+            nn.ReLU(),
+            nn.Dropout(self.p),
+
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(256, 512, 3),
+            nn.ReLU(),
+            nn.Dropout(self.p),
+
+            nn.MaxPool2d(3, 3)
+        )
+
+        # Compute flattened feature size after convs
+        self.flattened_size = 512*8*8 
+
+        self.classifier = nn.Sequential(
+            nn.Linear(self.flattened_size, 512),
+            nn.ReLU(),
+            nn.Dropout(self.p),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(self.p),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Dropout(self.p),
             nn.Linear(128, num_classes)
         )
         
