46c3a81e84bf2ec89885a8962f0a92359efaadb6
goicoechea.147720 <goicoechea.147720@e.unavarra.es>
2025-10-06 11:26:29 +0200

    removed batch normalization

--- weather_classification/modeling/binary_classifier.py
+++ weather_classification/modeling/binary_classifier.py
@@ -15,43 +15,34 @@ class BinaryWeatherClassifier_HighRes(nn.Module):
         self.features = nn.Sequential(
             # Block 1: 224x224 -> 112x112
             nn.Conv2d(3, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Conv2d(32, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
             
             # Block 2: 112x112 -> 56x56
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
             
             # Block 3: 56x56 -> 28x28
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
             
             # Block 4: 28x28 -> 14x14
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
             
             # Block 5: 14x14 -> 7x7
             nn.Conv2d(256, 512, kernel_size=3, padding=1),
-            nn.BatchNorm2d(512),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
         )
